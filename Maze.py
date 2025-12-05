475239c6de11b2b95cb7444d55d0360a82e9a079
dhrubojoutidas <dhrubotroyee20@gmail.com>
2025-12-05 11:44:46 +0800

    Changed the name of GV.py to Viwer.py

--- Maze.py
+++ Maze.py
@@ -1,5 +1,6 @@
 
 
+import GV
 
 EMPTY = 0
 WALL = 1
@@ -24,29 +25,9 @@ if __name__ == "__main__":
         [ WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL],
     ]
                     
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            
-            if grid[i][j] == EMPTY:
-                print("  ", end = "")
-                    
-            elif grid[i][j] == WALL:
-                print("##", end = "")
-                    
-            elif grid[i][j] == START:
-                print("^^", end = "")
-                    
-            elif grid[i][j] == END:
-                print("$$", end = "")
-                    
-            elif grid[i][j] == VISITED:
-                print("..", end = "")
-                    
-            else:
-                raise AssertionError
-            
-        print()
 
+
+    GV.view(grid)
 
     print("Find a solution to get from ^^ to $$, using the characters " 
         + "'" + NORTH + "', '" + EAST + "', '" + SOUTH + "' and '" + WEST + "'"
@@ -111,25 +92,6 @@ if __name__ == "__main__":
         print("MESSAGE 5") # Did not reach the end.
 
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            
-            if grid[i][j] == EMPTY:
-                print("  ", end = "")
-                    
-            elif grid[i][j] == WALL:
-                print("##", end = "")
-                    
-            elif grid[i][j] == START:
-                print("^^", end = "")
-                    
-            elif grid[i][j] == END:
-                print("$$", end = "")
-                    
-            elif grid[i][j] == VISITED:
-                print("..", end = "")
-                    
-            else:
-                raise AssertionError
-            
-        print()
+    
+# Second view
+    GV.view(grid)
