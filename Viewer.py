c70acc80933bbb93a00669cf607b29c92d233f9e
dhrubojoutidas <dhrubotroyee20@gmail.com>
2025-12-18 02:42:15 +0800

    Chnged maze display simbols

--- Viewer.py
+++ Viewer.py
@@ -8,11 +8,11 @@ def view(grid):
             if grid[i][j] == Maze.EMPTY:
                 print("  ", end="")
             elif grid[i][j] ==  Maze.WALL:
-                print("##", end="")
+                print("$$", end="")
             elif grid[i][j] ==  Maze.START:
                 print("^^", end="")
             elif grid[i][j] == Maze.END:
-                print("$$", end="")
+                print("##", end="")
             elif grid[i][j] ==  Maze.VISITED:
                 print("..", end="")
             else:
